5bca0d2e147c3ee176d58bfb1a63ff8cb6b49b17
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-01-25 16:59:35 +0200

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-  env: {
-    es2020: true,
-  },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended"],
   parserOptions: {
+    ecmaVersion: "latest",
     sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+    ],
   },
 };
