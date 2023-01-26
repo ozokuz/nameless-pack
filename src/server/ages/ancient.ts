c8e1230fa2914b7ed35b83ad162c5205d1e2807b
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-01-26 21:38:01 +0200

    harder andesite alloy & smoke logs to charcoal

--- src/server/ages/ancient.ts
+++ src/server/ages/ancient.ts
@@ -48,4 +48,69 @@ onEvent("recipes", (event) => {
       R: "#forge:dyes/red",
     },
   );
+
+  // Logs to charcoal
+  event.recipes.minecraft.smoking(
+    "minecraft:charcoal",
+    "#minecraft:logs_that_burn",
+  );
+
+  // Harder Andesite Alloy
+  event.remove({ output: "create:andesite_alloy" });
+  event.custom({
+    type: "tconstruct:casting_basin",
+    // @ts-expect-error custom
+    cast: {
+      item: "minecraft:andesite",
+    },
+    cast_consumed: true,
+    fluid: {
+      tag: "forge:molten_iron",
+      amount: 30,
+    },
+    result: "create:andesite_alloy",
+    cooling_time: 40,
+  });
+  event.custom({
+    type: "tconstruct:casting_basin",
+    // @ts-expect-error custom
+    cast: {
+      item: "minecraft:andesite",
+    },
+    cast_consumed: true,
+    fluid: {
+      tag: "forge:molten_zinc",
+      amount: 10,
+    },
+    result: "create:andesite_alloy",
+    cooling_time: 40,
+  });
+  event.custom({
+    type: "tconstruct:casting_basin",
+    // @ts-expect-error custom
+    cast: {
+      item: "tconstruct:seared_stone",
+    },
+    cast_consumed: true,
+    fluid: {
+      tag: "forge:molten_iron",
+      amount: 30,
+    },
+    result: "create:andesite_alloy",
+    cooling_time: 40,
+  });
+  event.custom({
+    type: "tconstruct:casting_basin",
+    // @ts-expect-error custom
+    cast: {
+      item: "tconstruct:seared_stone",
+    },
+    cast_consumed: true,
+    fluid: {
+      tag: "forge:molten_zinc",
+      amount: 10,
+    },
+    result: "create:andesite_alloy",
+    cooling_time: 40,
+  });
 });
