cd45333c732452982f1bb5a391ccc4e79a041103
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-01-26 21:02:12 +0200

    remove magical material conversions

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,4 @@ import "./server/settings";
 import "./server/remove";
 import "./server/food";
 import "./server/ages";
+import "./server/conversions";
---
+++ src/server/conversions.ts
@@ -0,0 +1,23 @@
+onEvent("recipes", (event) => {
+  // Disable Metal Conversions Without Melting
+  event.remove({
+    type: "minecraft:crafting_shapeless",
+    input: "#forge:ingots",
+    output: "#forge:storage_blocks",
+  });
+  event.remove({
+    type: "minecraft:crafting_shapeless",
+    input: "#forge:ingots",
+    output: "#forge:nuggets",
+  });
+  event.remove({
+    type: "minecraft:crafting_shapeless",
+    input: "#forge:storage_blocks",
+    output: "#forge:ingots",
+  });
+  event.remove({
+    type: "minecraft:crafting_shapeless",
+    input: "#forge:nuggets",
+    output: "#forge:ingots",
+  });
+});
