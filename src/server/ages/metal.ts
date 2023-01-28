4d2bed2acc12e4f1c508ad3cfe4e8f1884834a21
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-01-28 02:33:10 +0200

    multiblocks
    recipes for:
    - coke brick
    - blast brick
    - kiln brick
    - smeltery controller

--- src/server/ages/metal.ts
+++ src/server/ages/metal.ts
@@ -162,4 +162,59 @@ onEvent("recipes", (event) => {
     ],
     event,
   );
+
+  // Coke Brick
+  event.remove({ output: "immersiveengineering:cokebrick" });
+  event.recipes.minecraft.crafting_shaped(
+    // @ts-expect-error multiple
+    "3x immersiveengineering:cokebrick",
+    ["CBC", "BSB", "CBC"],
+    {
+      C: "minecraft:clay_ball",
+      B: "immersive_weathering:deepslate_brick",
+      S: "#forge:sandstone",
+    },
+  );
+
+  // Blast Brick
+  event.remove({ output: "immersiveengineering:blastbrick" });
+  // @ts-expect-error docs
+  event.recipes.tconstruct
+    .casting_basin(
+      "immersiveengineering:blastbrick",
+      "tconstruct:seared_stone",
+      1000,
+    )
+    .cast("minecraft:bricks")
+    .consumeCast()
+    .coolingTime(40);
+
+  // Kiln Brick
+  event.remove({ output: "immersiveengineering:alloybrick" });
+  event.recipes.minecraft.crafting_shaped(
+    // @ts-expect-error multiple
+    "2x immersiveengineering:alloybrick",
+    ["SB", "BS"],
+    {
+      S: "#forge:sandstone",
+      B: "minecraft:nether_brick",
+    },
+  );
+
+  // Smeltery Controller
+  event.remove({ output: "tconstruct:smeltery_controller" });
+  event.custom({
+    // @ts-expect-error retextured
+    type: "tconstruct:retextured_casting_basin",
+    cast: {
+      tag: "tconstruct:smeltery_bricks",
+    },
+    cast_consumed: true,
+    fluid: {
+      tag: "forge:molten_rose_gold",
+      amount: 360,
+    },
+    result: "tconstruct:smeltery_controller",
+    cooling_time: 100,
+  });
 });
