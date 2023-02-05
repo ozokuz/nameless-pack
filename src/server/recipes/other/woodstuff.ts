c34e322b42d34ef4d3a10c2de289ed6fceb932f9
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-02-05 23:26:27 +0200

    update create woodworking

--- src/server/recipes/other/woodstuff.ts
+++ src/server/recipes/other/woodstuff.ts
@@ -1,6 +1,21 @@
 export default (event: Internal.RecipeEventJS) => {
-  // Remove Create Log Sawing
-  event.remove({ output: "#minecraft:planks", type: "create:cutting" });
+  // Nerf Create Log Sawing
+  event.forEachRecipe(
+    {
+      output: "#minecraft:planks",
+      type: "create:cutting",
+    },
+    (recipe) => {
+      const plank = recipe.originalRecipeResult.withCount(4);
+      const log = recipe.originalRecipeIngredients[0]!;
+
+      event.recipes.create.cutting(plank, log);
+      event.remove({
+        output: recipe.originalRecipeResult,
+        type: "create:cutting",
+      });
+    },
+  );
 
   // Making slabs from planks in create's mechanical saw
   event.forEachRecipe(
@@ -18,5 +33,5 @@ export default (event: Internal.RecipeEventJS) => {
 
   // Making sticks from slabs in create's mechanical saw
   // @ts-expect-error multiple
-  event.recipes.create.cutting("6x minecraft:stick", "#minecraft:wooden_slabs");
+  event.recipes.create.cutting("2x minecraft:stick", "#minecraft:wooden_slabs");
 };
