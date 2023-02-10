86140dd34da4b17bfc3276afcc0f0a9c93ac7436
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-02-10 18:21:14 +0200

    temporarily disable ars noveau's robes

--- src/server/recipes/magic/ars_nouveau.ts
+++ src/server/recipes/magic/ars_nouveau.ts
@@ -14,4 +14,12 @@ export default (event: Internal.RecipeEventJS) => {
       I: "immersiveengineering:ingot_steel",
     },
   );
+
+  // Disable Ars Nouveau's Robes temporarily
+  ["novice", "apprentice", "archmage"].forEach((level) => {
+    ["boots", "leggings", "robes", "hood"].forEach((type) => {
+      // @ts-expect-error dynamic
+      event.remove({ output: `ars_nouveau:${level}_${type}` });
+    });
+  });
 };
