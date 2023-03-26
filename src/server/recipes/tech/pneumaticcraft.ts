2166782925fcf25079e140e34e965fcc8084c3ac
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-03-26 02:01:39 +0200

    gate more of pneumaticcraft behing immersive engineering

--- src/server/recipes/tech/pneumaticcraft.ts
+++ src/server/recipes/tech/pneumaticcraft.ts
@@ -7,4 +7,17 @@ export default (event: Internal.RecipeEventJS) => {
   // Remove PneumaticCraft's Thermopneumatic Processing Plant
   event.remove({ type: "pneumaticcraft:thermo_plant" });
   event.remove({ id: "pneumaticcraft:thermopneumatic_processing_plant" });
+
+  // Gated Reinforced Stone
+  event.remove({ id: "pneumaticcraft:reinforced_stone" });
+  event.recipes.minecraft.crafting_shaped(
+    // @ts-expect-error multiple
+    "4x pneumaticcraft:reinforced_stone",
+    ["ISI", "SCS", "ISI"],
+    {
+      I: "#forge:ingots/compressed_iron",
+      S: "minecraft:stone",
+      C: "immersiveengineering:steel_scaffolding_standard",
+    },
+  );
 };
